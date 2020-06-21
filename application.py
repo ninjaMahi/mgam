c8d4d7028ba5be2c03ab453b4844323307c2b1bb
Mahi Sarpal <mahisarpal@mahis-mbp.home>
2020-06-21 18:36:54 +0100

    app file chnages

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
 from cs50 import SQL
 
-
+#test
 # Configure application
 app = Flask(__name__)
 
